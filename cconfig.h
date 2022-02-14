be28bcb3c35d41fb00ff1b0c7f468c49053db252
NicolaGiardino <nicoladgg@protonmail.com>
2022-02-14 17:14:13 +0100

    Sdcard devdrivers

---
+++ cconfig.h
@@ -0,0 +1,21 @@
+#ifndef CCONFIG_H__
+#define CCONFIG_H__
+/* Automatically generated file; DO NOT EDIT. */
+/* compiler configure file for RT-Thread in GCC*/
+
+#define HAVE_NEWLIB_H 1
+#define LIBC_VERSION "newlib 3.0.0"
+
+#define HAVE_SYS_SIGNAL_H 1
+#define HAVE_SYS_SELECT_H 1
+#define HAVE_PTHREAD_H 1
+
+#define HAVE_FDSET 1
+#define HAVE_SIGACTION 1
+#define HAVE_SIGEVENT 1
+#define HAVE_SIGINFO 1
+#define HAVE_SIGVAL 1
+#define GCC_VERSION_STR "7.3.1 20180622 (release) [ARM/embedded-7-branch revision 261907]"
+#define STDC "2011"
+
+#endif
